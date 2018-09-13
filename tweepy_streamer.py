d805fcca8b963e7aa16f12a0a58a6736e070e2a9
bluefish168 <43221969+bluefish168@users.noreply.github.com>
2018-09-12 17:13:56 -0700

    first commit

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -11,7 +11,7 @@ import numpy as np
 import pandas as pd
 import re
 
-class TwitterClient():
+class TwitterClient(): 
     def __init__(self, twitter_user=None):
         self.auth = TwitterAuthenticator().authenticate_twitter_app()
         self.twitter_client = API(self.auth)
